532ee9d38caaf58e464299a61eea3c20255cc413
JPaul23 <73589069+JPaul23@users.noreply.github.com>
2022-02-01 16:03:25 +0200

    Client side Auth

--- chatApp-medical/client/src/App.jsx
+++ chatApp-medical/client/src/App.jsx
@@ -3,14 +3,19 @@ import { StreamChat } from 'stream-chat';
 import { Chat } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
 
-import { ChannelContainer, ChannelListContainer } from './components';
+import { ChannelContainer, ChannelListContainer, Auth } from './components';
 
 import './App.css';
 const apiKey = '6turu3yjmq7r';
 
 const client = StreamChat.getInstance(apiKey);
 
+const authToken = false;
+
 const App = () => {
+
+  if (!authToken) return <Auth />
+
   return (
     <div className='app__wrapper'>
       <Chat client={client} theme="team light">
