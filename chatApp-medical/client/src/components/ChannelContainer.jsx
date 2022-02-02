3cf354290cba5730983e8e61347c7e1ae3b70a3d
JPaul23 <73589069+JPaul23@users.noreply.github.com>
2022-02-02 10:03:18 +0200

    Channel Container

--- chatApp-medical/client/src/components/ChannelContainer.jsx
+++ chatApp-medical/client/src/components/ChannelContainer.jsx
@@ -1,9 +1,42 @@
 import React from "react";
+import { Channel, useChatContext, MessageTeam } from "stream-chat-react";
 
-const ChannelContainer = () => {
+import { ChannelInner, CreateChannel, EditChannel, TeamMessage } from './'
+const ChannelContainer = ({ isCreating, setIsCreating, isEditing, setIsEditing, createType }) => {
+    const { channel } = useChatContext(); //information of the current channel
+
+    if (isCreating) {
+        return (
+            <div className="channel__container">
+                <CreateChannel createType={createType} setIsCreating={setIsCreating} />
+            </div>
+        )
+    }
+
+    if (isEditing) {
+        return (
+            <div className="channel__container">
+                <EditChannel setIsEditing={setIsEditing} />
+            </div>
+        )
+    }
+
+    //empty chat with no messages yet
+    const EmptyState = () => (
+        <div className="channel-empty__container">
+            <p className="channel-empty__first">This is the beginning of your chat history</p>
+            <p className="channel-empty__second">Send messages, attachements, links, emojis, and more!</p>
+        </div>
+    )
+
     return (
-        <div>
-            ChannelContainer
+        <div className="channel__container">
+            <Channel
+                EmptyStateIndicator={EmptyState}
+                Message={(messageProps, i) => <MessageTeam key={i} {...messageProps} />}
+            >
+                <ChannelInner setIsEditing={setIsEditing} />
+            </Channel>
         </div>
     )
 };
