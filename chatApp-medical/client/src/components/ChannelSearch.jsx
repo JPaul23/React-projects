ec16e901225bb177557b29d15a0cd6ca6b79e1e7
JPaul23 <73589069+JPaul23@users.noreply.github.com>
2022-02-25 18:21:50 +0200

    Channel & user Search

--- chatApp-medical/client/src/components/ChannelSearch.jsx
+++ chatApp-medical/client/src/components/ChannelSearch.jsx
@@ -1,15 +1,44 @@
 import React, { useState, useEffect } from 'react';
 import { getChannel, useChatContext } from 'stream-chat-react';
 
+import { ResultsDropdown } from '.';
 import { SearchIcon } from '../assets';
 
-const ChannelSearch = () => {
+const ChannelSearch = ({setToggleContainer}) => {
+    const {client, setActiveChannel } = useChatContext();
     const [query, setQuery] = useState('');
     const [loading, setLoading] = useState(false);
+    const [teamChannels, setTeamChannels] = useState([]) //which currently active team channels
+    const [directChannels, setDirectChannels] = useState([]) //which currently active direct channels
+
+    useEffect(() => {
+        if(!query){
+            // no query clear these
+            setTeamChannels([]);
+            setDirectChannels([]);
+        }
+
+    }, [query]) //call the function everytime the query changes
 
     const getChannels = async (text) => {
         try {
-            //TODO: fetch channels
+            // query our channel
+            const channelResponse = client.queryChannels({
+                type:'team', 
+                name:{ $autocomplete: text}, 
+                members: {$in: [client.userID] } // includes userID
+            });
+            const userResponse = client.queryUsers({
+                //exclude current userID, not finding ourself in search
+                id: { $ne: client.userID },
+                name:{ $autocomplete: text},  
+            });
+
+            const[ channels, {users}] = await Promise.all([ channelResponse, userResponse]); // requesting happening simultaneosly 
+            if(channels.length) setTeamChannels(channels);
+            if(users.length) setDirectChannels(users);
+
+
         } catch (error) {
             setQuery('')
         }
@@ -21,6 +50,11 @@ const ChannelSearch = () => {
         setLoading(true);
         setQuery(event.target.value);
         getChannels(event.target.value);
+    }
+
+    const setChannel = (channel) => {
+        setQuery('');
+        setActiveChannel(channel);
     }
 
     return (
@@ -36,6 +70,17 @@ const ChannelSearch = () => {
                     onChange={onSearch}
                 />
             </div>
+
+{ query && (
+    <ResultsDropdown 
+        teamChannels={teamChannels}
+        directChannels={directChannels}
+        loading={loading}
+        setChannel = {setChannel}
+        setQuery={setQuery}
+        setToggleContainer={setToggleContainer}
+    />
+)}
 
         </div>
     );
